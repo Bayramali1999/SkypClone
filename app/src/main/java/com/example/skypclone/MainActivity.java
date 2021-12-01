5c5f861dab2dcfb60592e03bf5147286f4c97e95
Bayramali1999 <babdiyev361@gmail.com>
2021-12-01 23:33:38 +0500

    all works

--- app/src/main/java/com/example/skypclone/MainActivity.java
+++ app/src/main/java/com/example/skypclone/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.skypclone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.skypclone.activities.NotificationActivity;
+import com.example.skypclone.activities.RegisterActivity;
+import com.example.skypclone.activities.SettingActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,10 +17,16 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.skypclone.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private BottomNavigationView navView;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +35,10 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(listener);
+
+        mAuth = FirebaseAuth.getInstance();
 //
 //        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
 //                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
@@ -33,4 +48,32 @@ public class MainActivity extends AppCompatActivity {
 //        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_setting:
+                    Intent settingIntent = new Intent(MainActivity.this, SettingActivity.class);
+                    startActivity(settingIntent);
+                    break;
+                case R.id.navigation_home:
+                    Intent homeIntent = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(homeIntent);
+                    break;
+                case R.id.navigation_notifications:
+                    Intent nIntent = new Intent(MainActivity.this, NotificationActivity.class);
+                    startActivity(nIntent);
+                    break;
+
+                case R.id.navigation_logout:
+                    mAuth.signOut();
+                    Intent rIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                    startActivity(rIntent);
+                    finish();
+                    break;
+
+            }
+            return false;
+        }
+    };
 }
